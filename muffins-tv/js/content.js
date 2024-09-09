7c535a1de6fab7ccaa353d88396a101387009c61
Aderito-Muffins <mufumeaderito@gmail.com>
2024-09-09 05:09:13 -0700

    fix

--- muffins-tv/js/content.js
+++ muffins-tv/js/content.js
@@ -156,7 +156,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                         </div>
                         <div class="gen-info-contain">
                             <div class="gen-movie-info">
-                                <h3><a href="#">${film.title} (${film.title_original})</a></h3>
+                                <h3><a href="#">${film.title}</a></h3>
                             </div>
                             <div class="gen-movie-meta-holder">
                                 <ul>
@@ -172,6 +172,68 @@ document.addEventListener("DOMContentLoaded", async function () {
         `).join('');
     }
 
+    function createTvItems(films) {
+        if (!Array.isArray(films)) {
+            console.error('O objeto de filmes não é um array:', films);
+            return '';
+        }
+        return films.map(film => `
+        <div class="item">
+            <div class="movie type-movie status-publish has-post-thumbnail hentry movie_genre-${film.type.toLowerCase()}">
+                <div class="gen-carousel-movies-style-2 movie-grid style-2">
+                    <div class="gen-movie-contain">
+                        <div class="gen-movie-img">
+                            <img src="${film.thumb}" alt="Movie Thumbnail">
+                            <div class="gen-movie-add">
+                                <div class="wpulike wpulike-heart">
+                                    <div class="wp_ulike_general_class wp_ulike_is_not_liked">
+                                        <button type="button" class="wp_ulike_btn wp_ulike_put_image"></button>
+                                    </div>
+                                </div>
+                                <ul class="menu bottomRight">
+                                    <li class="share top">
+                                        <i class="fa fa-share-alt"></i>
+                                        <ul class="submenu">
+                                            <li><a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                            <li><a href="#" class="instagram"><i class="fab fa-instagram"></i></a></li>
+                                            <li><a href="#" class="twitter"><i class="fab fa-twitter"></i></a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                                <div class="movie-actions--link_add-to-playlist dropdown">
+                                    <a class="dropdown-toggle" href="#" data-toggle="dropdown"><i class="fa fa-plus"></i></a>
+                                    <div class="dropdown-menu mCustomScrollbar">
+                                        <div class="mCustomScrollBox">
+                                            <div class="mCSB_container">
+                                                <a class="login-link" href="register.html">Sign in to add this movie to a playlist.</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="gen-movie-action">
+                                <a href="/single-tv.html?id=${film.id}" class="gen-button">
+                                    <i class="fa fa-play"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="gen-info-contain">
+                            <div class="gen-movie-info">
+                                <h3><a href="#">${film.title}</a></h3>
+                            </div>
+                            <div class="gen-movie-meta-holder">
+                                <ul>
+                                    <li>Live</li>
+                                    <li><a href="#"><span>auto</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `).join('');
+    }
     async function fetchAndProcessData(url) {
         try {
             const response = await fetch(url, {
@@ -263,6 +325,25 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
     }
 
+    function updateTvCarousel(movies) {
+        const tvItems = createTvItems(movies);
+        updateDOM("#tv-carossel", tvItems, {
+            loop: true,
+            dots: false,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 6000,
+            margin: 30,
+            responsive: {
+                0: { items: 1, nav: true },
+                576: { items: 2, nav: false },
+                768: { items: 3, nav: true, loop: true },
+                992: { items: 4, nav: true, loop: true },
+                1200: { items: 5, nav: true, loop: true }
+            }
+        });
+    }
+
     function updateBannerCarousel(data) {
         const carouselHtml = createCarouselItems(data);
         updateDOM("#movie-carousel", carouselHtml, {
@@ -288,6 +369,12 @@ document.addEventListener("DOMContentLoaded", async function () {
         loadMoviesWithCache(url, 'allTimeHitsCache', updateCarousel);
     }
 
+    async function loadTv() {
+        const url = `http://localhost:3000/muffins/v1/tv/list?limit=${limit}&page=${page}&offset=${offset}`;
+        loadMoviesWithCache(url, 'tvItemsCache', updateTvCarousel);
+    }
+
     loadBannerMovies();
     loadAllTimeHits();
+    loadTv();
 });
